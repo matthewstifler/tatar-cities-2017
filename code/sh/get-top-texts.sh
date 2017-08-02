1262dc33b65dfbc34fc94fe2ce9f4c35b53df5a2
mishaulitskiy <mtwstflr@gmail.com>
2017-08-02 23:37:00 +0000

    Added output as zip, fixed a mistake in the usage example

--- code/sh/get-top-texts.sh
+++ code/sh/get-top-texts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Usage: bash 100 50 lda-reports/elabuga-lda-100/elabuga-lda-100-doc-topic.csv data/data-elabuga-processed.csv elabuga-100
+# Usage: bash ./sh/get-top-texts.sh 100 50 lda-reports/elabuga-lda-100/elabuga-lda-100-doc-topics.csv data/data-elabuga-processed.csv elabuga-100
 # Run from project root
 
 # TODO: Automatically identify number of topics (N of columns in doc-topic matrix, i.e. number of commas + 1)
@@ -13,10 +13,14 @@ docTopic=$3
 textSource=$4
 dirName=$5
 
-mkdir dirName && cd $_
+mkdir $dirName && cd $_
 
 for ((i=1; i<=$nOfTopics; i++))
   do
     Rscript --vanilla ../code/R/get-top-texts-for-a-topic.R ../$docTopic ../$textSource $i $nOfPosts > "topic-$i.txt"
  done
+ 
+cd ..
+tar -zcvf "$dirName.tar.gz" ./$dirName
+rm -r $dirName
  
