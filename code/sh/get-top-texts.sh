43c45bf598211e2c04b388867e570520c95c3fc7
mishaulitskiy <mtwstflr@gmail.com>
2017-08-02 18:22:51 +0000

    Script to automatically generate N top texts for each topic

--- code/sh/get-top-texts.sh
+++ code/sh/get-top-texts.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-# Usage: bash
+# Usage: bash 100 50 lda-reports/elabuga-lda-100/elabuga-lda-100-doc-topic.csv data/data-elabuga-processed.csv
+# Run from project root
 
+nOfTopics=$1
+nOfPosts=$2
+docTopic=$3
+textSource=$4
+
+mkdir top-texts-lda && cd $_
+
+for ((i=1; i<=$nOfTopics; i++))
+  do
+    Rscript --vanilla ../code/R/get-top-texts-for-a-topic.R ../$docTopic ../$textSource $i $nOfPosts > "topic-$i.txt"
+ done
+ 
