d5d038bbd2748b847fd1dda04f92653d820d7595
mishaulitskiy <mtwstflr@gmail.com>
2017-08-27 19:38:04 +0000

    Removed obsolete functionality from Rscript calls in main bash file

--- code/sh/main.sh
+++ code/sh/main.sh
@@ -27,10 +27,7 @@ for ((i=6; i<=$#; i++))
 
 #----------Doing the deeds----------
 
-Rscript --vanilla code/R/02-processing-text-data.R $dataSource $cleanedFilePath $stoplistPath &
-
-wait $!
-
+Rscript --vanilla code/R/02-processing-text-data.R $dataSource $cleanedFilePath $stoplistPath
 Rscript --vanilla code/R/03-1-lda-speedreader.R $cleanedFilePath $reportPrefix $stoplistPath $nOfCores "${topicsArray[@]}" # Some bash hack to print array
 python code/py/get-top-texts.py $pathToDocTopics $dataSource $reportPrefix
 
